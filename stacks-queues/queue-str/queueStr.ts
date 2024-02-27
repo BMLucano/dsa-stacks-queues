a41abaf1da3fd2a71c2858d59018876343ff5cf5
Carl Molina <carlandrew.molina@gmail.com>
2024-02-27 11:43:04 -0800

    first draft of QueueStr class

--- stacks-queues/queue-str/queueStr.ts
+++ stacks-queues/queue-str/queueStr.ts
@@ -1,30 +1,37 @@
 /** QueueStr: can remove from start or add to end */
 
+import { IndexError } from "../common/ll";
+
 class QueueStr {
+  _array: string[];
 
   constructor(initial: string[] = []) {
+    this._array = initial;
   }
 
   /** enqueue(val): add val to end. Returns undefined. */
   enqueue(val: string): void {
+    this._array.push(val);
   }
 
   /** dequeue(): remove & return item from start.
    * Throws error if empty. */
   dequeue(): string {
-    return "x";
+    if (this._array.length === 0) throw new IndexError();
+    return this._array.shift()!;
   }
 
   /** peek(): return the value of top. */
   peek(): string {
-    return "x";
+    if (this._array.length === 0) throw new IndexError();
+    return this._array[0];
   }
 
-  /** peek(): return the value of top. */
+  /** : return true if queue is empty. */
   isEmpty(): boolean {
-    return false;
+    return this._array.length === 0;
   }
-}
 
+}
 
 export { QueueStr };
