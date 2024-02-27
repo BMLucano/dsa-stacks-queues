9eb4ab7fac655ec35c37aeccfcf8d0a4c7a56892
Brandie Lucano <blucano.dev@gmail.com>
2024-02-26 18:00:21 -0800

    StackStr works and tests passing

--- stacks-queues/stack-str/stackStr.ts
+++ stacks-queues/stack-str/stackStr.ts
@@ -3,28 +3,35 @@ import { IndexError } from "../common/ll";
 /** StackStr: can remove from the top or add to the top. */
 
 class StackStr {
+  _array: string[];
 
   constructor(initial: string[] = []) {
+    this._array = initial
   }
 
   /** push(val): add val to top. Returns undefined. */
   push(val: string): void {
+    this._array.push(val);
   }
 
   /** pop(): remove and return item from top.
    * Throws error if empty. */
   pop(): string {
-    return "x";
+    if(this._array.length === 0) throw new IndexError;
+
+    return this._array.pop()!;
   }
 
   /** peek(): return the value of first item. */
   peek():string {
-    return "x";
+    if(this._array.length === 0) throw new IndexError;
+
+    return this._array[this._array.length-1];
   }
 
-  /** peek(): return the value of top. */
+  /** isEmpty(): return true if list is empty */
   isEmpty():boolean {
-    return false;
+    return this._array.length === 0;
   }
 }
 
